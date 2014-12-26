6e19518e13a4cfa1576246906e2224c18a94b66e
Yrjö Kari-Koskinen <ykk@peruna.fi>
2014-12-27 00:45:25 +0200

    simple prompt and execute

--- src/cardgame/commands.clj
+++ src/cardgame/commands.clj
@@ -2,10 +2,26 @@
     "Commands that can be issued in the prompt"
     (:require [cardgame.decks.core :as decks]))
 
-
 (defn create-game [name number-of-players decktype]
     (let [deck (decks/get-deck decktype)]
         (when deck
             {:name name
              :number-of-players number-of-players
              :deck (shuffle deck)})))
+
+(defn execute [prompt-prefix username command-line]
+    "Execute given command if it is found in local namespace with a correct arity. Return the new prompt-prefix."
+    (let [parts (clojure.string/split command-line #" ")
+          command (first parts)
+          params (rest parts)
+          command-fun (resolve (symbol "cardgame.commands" command))]
+        (when-not (= "quit" command)
+            (if-not command-fun
+                (do (println (str "Command '" command "' not found."))
+                    prompt-prefix)
+                (let [arities (->> command-fun meta :arglists (map count))]
+                    (if (= -1 (.indexOf arities (count params)))
+                        (do (println (str "Wrong number of arguments for '" command "'."))
+                            prompt-prefix)
+                        (apply command-fun params)))))))
+
--- src/cardgame/prompt.clj
+++ src/cardgame/prompt.clj
@@ -1,5 +1,13 @@
 (ns cardgame.prompt
     (:require [cardgame.commands :as cmd]))
 
+(def prompt-symbol "> ")
 
+(defn prompt [username]
+    (loop [prompt-prefix ""]
+        (print (str prompt-prefix prompt-symbol))
+        (flush)
+        (let [new-prefix (cmd/execute prompt-prefix username (read-line))]
+            (when new-prefix
+                (recur new-prefix)))))
 
