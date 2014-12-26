7bc2bc321268bc79e74402055a300b4481c52774
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2014-12-26 16:36:29 +0200

    french deck

---
+++ src/cardgame/french_deck.clj
@@ -0,0 +1,40 @@
+(ns cardgame.french-deck
+  "The french deck"
+  (:gen-class))
+
+(def values "A23456789TJQK")
+(def suits "♠♡♢♣")
+
+(defn- cart [colls]
+  (if (empty? colls)
+    '(())
+    (for [x (first colls)
+          more (cart (rest colls))]
+      (cons x more))))
+
+(defn is-spade? [card]
+    (= \♠ (first card)))
+
+(defn is-diamonds? [card]
+    (= \♢ (first card)))
+
+(defn is-clubs? [card]
+    (= \♣ (first card)))
+
+(defn is-hearts? [card]
+    (= \♡ (first card)))
+
+(defn numeral-value [card]
+    (let [val (subs card 1)]
+        (case val
+            "A" 1
+            "T" 10
+            "J" 11
+            "Q" 12
+            "K" 13
+            (read-string val))))
+
+
+(defn get-deck []
+    (map #(reduce str %) (cart (list suits values))))
+
